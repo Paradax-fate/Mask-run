33ea52c0e0fc915591144b6bf1c1d3fe0ea6d05f
Javi117 <javierhernandezal98@gmail.com>
2026-01-31 18:00:43 -0500

    animación idle máscaras

--- Assets/Scripts/BossMaskController.cs
+++ Assets/Scripts/BossMaskController.cs
@@ -1,16 +1,41 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class BossMaskController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Boss Settings")]
+    [SerializeField] private float moveSpeed = 3.0f;
+    [SerializeField] private Transform maskRotator;
+    [SerializeField] private float minX = -0.7f;
+    [SerializeField] private float maxX = 0.7f;
+
+    [Header("Floating")]
+    [SerializeField] private float floatAmplitude = 0.5f; // altura
+    [SerializeField] private float floatFrequency = 2f;   // velocidad
+
+    private Vector3 startPosition;
+    private int direction = 1; // 1 = derecha, -1 = izquierda
     void Start()
     {
-        
+        startPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        IdleMovementMask();
+    }
+
+    void IdleMovementMask()
+    {
+             
+
+        // ─── Flotación vertical ───
+        float yOffset = Mathf.Sin(Time.time * floatFrequency) * floatAmplitude;
+
+        transform.position = new Vector3(
+            transform.position.x,
+            startPosition.y + yOffset,
+            transform.position.z
+        );
     }
 }
