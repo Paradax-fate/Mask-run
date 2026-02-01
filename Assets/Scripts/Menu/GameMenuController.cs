d234513b4771557c14ad981fc9179b6be06a8005
Paradax <paradax.bryan@gmail.com>
2026-02-01 18:13:22 -0500

    Fixing small bugs
    - When we pause and exit the main menu, the game will be still paused when we exit. Fixed.
    
    - When building the project the build failed because the UnityEditor class doesn't exist in the build context. Fixed

--- Assets/Scripts/Menu/GameMenuController.cs
+++ Assets/Scripts/Menu/GameMenuController.cs
@@ -55,6 +55,7 @@ public class GameMenuController : MonoBehaviour
 
     public void OnGoToMainButtonClicked()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("MainMenu");
     }
 
